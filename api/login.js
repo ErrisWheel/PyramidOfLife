fb2023df0f079ebab9f4efa790bf28626036c2fd
ErrisWheel <errisdelrosario@gmail.com>
2025-02-14 21:25:00 +0800

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,1 +1,19 @@
+export default async function handler(req, res) {
+    if (req.method === "POST") {
+        const { username, password } = req.body;
+      
+        const users = [{ username: "test", password: "1234" }];
 
+        const userExists = users.find(
+            (user) => user.username === username && user.password === password
+        );
+
+        if (userExists) {
+            res.status(200).json({ message: "Login successful!" });
+        } else {
+            res.status(401).json({ message: "Invalid credentials" });
+        }
+    } else {
+        res.status(405).json({ message: "Method Not Allowed" });
+    }
+}
