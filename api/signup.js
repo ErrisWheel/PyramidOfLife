e47b1ae146621d8510f1ec8438aa676641058181
ErrisWheel <errisdelrosario@gmail.com>
2025-02-17 20:27:29 +0800

    Update signup.js

--- api/signup.js
+++ api/signup.js
@@ -1,3 +1,18 @@
+import admin from "firebase-admin";
+import { getFirestore } from "firebase-admin/firestore";
+
+if (!admin.apps.length) {
+    admin.initializeApp({
+        credential: admin.credential.cert({
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        }),
+    });
+}
+
+const db = getFirestore();
+
 export default async function handler(req, res) {
     if (req.method === "POST") {
         const { username, password, email } = req.body;
